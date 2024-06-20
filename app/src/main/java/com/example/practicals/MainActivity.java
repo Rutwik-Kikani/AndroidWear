9f0a76cfa78493dba8d28173a962ced198f6c1fb
Rutwik Kikani <kikanirutwik001@gmail.com>
2024-06-20 00:58:21 -0400

    today's date

--- app/src/main/java/com/example/practicals/MainActivity.java
+++ app/src/main/java/com/example/practicals/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.practicals;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +19,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        TextView mTxtView = findViewById(R.id.txtDate);
+        Date today = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, MMMM d - yyyy");
+        mTxtView.setText("Today's date:\n"+dateFormat.format(today) );
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
