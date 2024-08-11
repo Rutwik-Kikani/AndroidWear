b7fc87f6c2038d921681674d1b13cd70748fab58
Rutwik Kikani <kikanirutwik001@gmail.com>
2024-08-11 02:29:24 -0400

    Confirmation activity in wear.

--- app/src/main/java/com/example/practicals/MainActivity.java
+++ app/src/main/java/com/example/practicals/MainActivity.java
@@ -2,11 +2,7 @@ package com.example.practicals;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.wear.widget.WearableLinearLayoutManager;
 
 import com.example.practicals.databinding.ActivityMainBinding;
@@ -21,8 +17,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        //setContentView(R.layout.activity_main);
         mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mainBinding.getRoot());
         init();
--- app/src/main/java/com/example/practicals/MenuAdapter.java
+++ app/src/main/java/com/example/practicals/MenuAdapter.java
@@ -1,29 +1,35 @@
 package com.example.practicals;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.wear.activity.ConfirmationActivity;
 
 import com.example.practicals.databinding.MenuItemsBinding;
 
 import java.util.List;
 
-public class MenuAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+public class MenuAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> implements View.OnClickListener {
     private List<MenuItem> mnuList;
-
+    Intent confirmIntent1;
+    Context context;
     MenuItemsBinding itemsBinding;
-    public MenuAdapter(List<MenuItem> itemList, Context context ) {
+
+    public MenuAdapter(List<MenuItem> itemList, Context context) {
         super();
         this.mnuList = itemList;
+        this.context = context;
     }
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    //return null;
+        //return null;
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         itemsBinding = MenuItemsBinding.inflate(layoutInflater, parent, false);
         return new ViewHolder(itemsBinding);
@@ -39,15 +45,35 @@ public class MenuAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
         return mnuList.size();
     }
 
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.menuIcon){
+            confirmIntent1 = new Intent(context, ConfirmationActivity.class);
+            confirmIntent1.putExtra(ConfirmationActivity.EXTRA_ANIMATION_TYPE, ConfirmationActivity.SUCCESS_ANIMATION);
+            confirmIntent1.putExtra(ConfirmationActivity.EXTRA_MESSAGE, "Succeeded!!");
+        }
+        else if(view.getId() == R.id.menuItem){
+            confirmIntent1 = new Intent(context, ConfirmationActivity.class);
+            confirmIntent1.putExtra(ConfirmationActivity.EXTRA_ANIMATION_TYPE, ConfirmationActivity.FAILURE_ANIMATION);
+            confirmIntent1.putExtra(ConfirmationActivity.EXTRA_MESSAGE, "Failed!!");
+        }
+        confirmIntent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(confirmIntent1);
+    }
+
     class ViewHolder extends RecyclerView.ViewHolder {
         MenuItemsBinding recyclerRowBinding;
-        public ViewHolder(MenuItemsBinding menuItemsBinding){
+
+        public ViewHolder(MenuItemsBinding menuItemsBinding) {
             super(menuItemsBinding.getRoot());
             this.recyclerRowBinding = menuItemsBinding;
         }
-        public void bindView(MenuItem objMenu){
+
+        public void bindView(MenuItem objMenu) {
             recyclerRowBinding.menuItem.setText(objMenu.getText());
             recyclerRowBinding.menuIcon.setImageResource(objMenu.getImage());
+            recyclerRowBinding.menuIcon.setOnClickListener(MenuAdapter.this);
+            recyclerRowBinding.menuItem.setOnClickListener(MenuAdapter.this);
         }
     }
 }
